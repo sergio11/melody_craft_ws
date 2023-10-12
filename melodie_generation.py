f1e2636ed34e70c1149d2491ba712fde0d1f9217
Sergio Sánchez Sánchez <sss4esob@gmail.com>
2023-10-12 20:24:07 +0200

    Refactoring generate melody method, send MIDI file over SSE

--- melodie_generation.py
+++ melodie_generation.py
@@ -1,4 +1,5 @@
 from audiocraft.models import MusicGen
+from .sse_server import send_sse_event
 
 # Average Words Per Minute (WPM) estimation
 AVERAGE_WPM = 150
@@ -19,39 +20,38 @@ def calculate_duration(text):
 
 def generate_melody(text):
     """
-    Generate a melody from the given text and return the generated tokens.
+    Generate a melody from the given text and send SSE notifications.
 
     Args:
         text (str): The text used to generate the melody.
-
-    Returns:
-        list: The generated tokens.
     """
     try:
-        print(f"Defining the text for the melody: {text}")
-        print("Loading the pre-trained model...")
+        send_sse_event("info", "Defining the text for the melody")
+        send_sse_event("info", "Loading the pre-trained model...")
         model = MusicGen.get_pretrained('facebook/musicgen-small')
 
         estimated_duration = calculate_duration(text)
-        print(f"Estimated duration of the melody: {estimated_duration} seconds")
+        send_sse_event("info", f"Estimated duration of the melody: {estimated_duration} seconds")
 
         generation_params = {
             'use_sampling': True,
             'top_k': 250,
             'duration': estimated_duration
         }
-        print(f"Setting generation parameters: {generation_params}")
+        send_sse_event("info", f"Setting generation parameters: {generation_params}")
         model.set_generation_params(**generation_params)
-        print("Generating the melody from the text...")
+        send_sse_event("info", "Generating the melody from the text...")
         output = model.generate(
             descriptions=[text],
             progress=True,
-            return_tokens=True
+            return_midi=True  # Request to return a MIDI file instead of tokens
         )
-        print(f"Generated tokens: {output[0]}")
 
-        return output[0]
+        # The MIDI file is in output['midi']
+        midi_file = output['midi']
+
+        # Send the MIDI file via SSE
+        send_sse_event("success", "Melody generated", midi_file)
 
     except Exception as e:
-        # You can handle the error here or raise it as needed
-        raise Exception(f"Error: {str(e)}")
+        send_sse_event("error", f"Error: {str(e)}")
